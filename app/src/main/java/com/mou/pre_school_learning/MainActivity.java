6efca1c90875935299ae006ffc0dc2a9faa7176a
mou-smartmux <ummemou1719@gmail.com>
2021-11-27 01:14:40 +0600

    my first commit

---
+++ app/src/main/java/com/mou/pre_school_learning/MainActivity.java
@@ -0,0 +1,14 @@
+package com.mou.pre_school_learning;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
